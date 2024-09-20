f750ae7b1bc214392ec79ff497694f576b71020a
7Flafleur <7nb@hotmail.fr>
2024-09-20 18:03:22 +0200

    tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+def add (a,b):
+    """
+    Add a and b together and return result
+    """
+    return a+b
+
+def subtract (a,b):
+
+    return a-b
---
+++ app/app/tests.py
@@ -0,0 +1,25 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """ Test the calc module"""
+
+    def test_add_numbers(self):
+        """ test adding numbers together """
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+    
+    def test_subtract_numbers(self):
+        """ TEst subtracting one number from another"""
+        res = calc.subtract(2,1)
+
+        self.assertEqual(res,1)
+
+
