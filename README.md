d6f388a832941e2fd9a70f3beca93871f7df9310
7Flafleur <7nb@hotmail.fr>
2024-09-22 19:21:31 +0200

    wait_for_db

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Course code for: Build a Backend REST API with Python &amp; Django - Advanced: Take the course here: https://londonapp.dev/c2
 
 Run commands in container : docker compose run --rm app sh -c PLUS command
+
+"python manage.py test"
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,29 @@
 """
 Django command to wait for database to be available
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        """empty stub to create command"""
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                """ check raises exceptions mentioned below unless db is ready,code enters except part,
+                following line is only executed if exceptions were not raised """
+                db_up = True 
+            except(Psycopg2Error,OperationalError):
+                self.stdout.write('Database unavailable, waiting  second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))
+
 
