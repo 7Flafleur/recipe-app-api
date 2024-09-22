19a67007ae868c24647d1b2e303cb3627874257c
7Flafleur <7nb@hotmail.fr>
2024-09-22 14:10:17 +0200

    correct files name error

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,13 +1,12 @@
 """
 Django command to wait for database to be available
 """
-
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         """empty stub to create command"""
         pass
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -15,10 +15,11 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """ Test commands."""
 
-    def test_wait_for_db_ready(self,patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for database if database ready."""
-        patched_check.return_value=True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(database=['default'])
+
