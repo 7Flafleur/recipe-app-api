0ebf0882362ba6f940cb9229136beba10f00d0ca
7Flafleur <7nb@hotmail.fr>
2024-09-23 14:05:20 +0200

    superuser creation ok

--- app/core/models.py
+++ app/core/models.py
@@ -16,15 +16,14 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError("User must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+            raise ValueError('User must have an email address')
+        user = self.model(email = self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password,**extra_fields):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
@@ -36,7 +35,6 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
-
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -44,5 +42,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name"]
+    USERNAME_FIELD = 'email'
