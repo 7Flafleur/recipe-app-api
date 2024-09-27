af42b7fc6e5b2d949fee01e932be1243d00daaed
7Flafleur <7nb@hotmail.fr>
2024-09-27 15:46:28 +0200

    recipe app setup

--- app/core/admin.py
+++ app/core/admin.py
@@ -48,3 +48,4 @@ class UserAdmin(BaseUserAdmin):
 
 """ Apply UserAdmin model for display"""
 admin.site.register(models.User,UserAdmin)
+admin.site.register(models.Recipe)
--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,17 @@
 Database models.
 """
 
+import uuid
+import os
+
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 
 class UserManager(BaseUserManager):
@@ -43,3 +48,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # tags = models.ManyToManyField('Tag')
+    # ingredients = models.ManyToManyField('Ingredient')
+    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
